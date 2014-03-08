6f91c87685ed562a2b904d989ac881108a8e4850
Alec Geatches <throwpi@me.com>
2014-03-07 17:58:43 -0800

    Added Base58 encoding and tests for keys without zeroed bytes in the front

--- base58.go
+++ base58.go
@@ -1,5 +1,27 @@
 package mercer
 
-func Encode(bytes []byte) string {
-	return ""
+import (
+	"bytes"
+	"math/big"
+)
+
+func Encode(b []byte) string {
+	base58characters := "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+	buffer := new(bytes.Buffer)
+
+	x := new(big.Int).SetBytes(b)
+	for x.Cmp(big.NewInt(0)) == 1 {
+		var remainder *big.Int
+		x, remainder = x.DivMod(x, big.NewInt(58), new(big.Int))
+		buffer.WriteByte(base58characters[remainder.Uint64()])
+	}
+	return reverse(buffer.String())
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
--- base58_test.go
+++ base58_test.go
@@ -1,13 +1,36 @@
 package mercer
 
 import (
+	"encoding/hex"
 	"testing"
 )
 
 func TestBase58EncodingOfAnEmptySliceIsAnEmptyString(t *testing.T) {
-	bytes :=  make([]byte, 0)
+	bytes := make([]byte, 0)
 	encoded := Encode(bytes)
 	if encoded != "" {
 		t.Errorf("Expected empty string, got '%s'", encoded)
+	}
+}
+
+func TestBase58EncodingOf0x01Is2(t *testing.T) {
+	bytes, err := hex.DecodeString("01")
+	if err != nil {
+		t.Fatalf("Error creating bytes from hex string")
+	}
+	encoded := Encode(bytes)
+	if encoded != "2" {
+		t.Errorf("Expected '2', got '%s'", encoded)
+	}
+}
+
+func TestBase58EncodingOf0xffIs5Q(t *testing.T) {
+	bytes, err := hex.DecodeString("ff")
+	if err != nil {
+		t.Fatalf("Error creating bytes from hex string")
+	}
+	encoded := Encode(bytes)
+	if encoded != "5Q" {
+		t.Errorf("Expected '5Q', got '%s'", encoded)
 	}
 }
